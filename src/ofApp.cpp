a5a65d4de924d4711057231318d6ed026f8ee5c5
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2016-11-27 14:01:30 +0100

    Removed comments from ofApp.cpp

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -3,14 +3,8 @@
 void ofApp::setup(){
 	ofBackground(0);
 
-	// Enable or disable audio for video sources globally
-	// Set this to false to save resources on the Raspberry Pi
 	ofx::piMapper::VideoSource::enableAudio = false;
 
-	// Add our CustomSource to list of fbo sources of the piMapper
-	// FBO sources should be added before piMapper.setup() so the
-	// piMapper is able to load the source if it is assigned to
-	// a surface in XML settings.
 	crossSource = new CrossSource();
 	customSource = new CustomSource();
 	spritesheetSource = new SpritesheetSource();
@@ -20,9 +14,6 @@ void ofApp::setup(){
 	piMapper.registerFboSource(spritesheetSource);
 	
 	piMapper.setup();
-
-	// The info layer is hidden by default, press <i> to toggle
-	// piMapper.showInfo();
 	
 	ofSetFullscreen(Settings::instance()->getFullscreen());
 	
